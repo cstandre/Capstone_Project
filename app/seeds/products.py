5d3f78b7bc3b68ef00dc6956889bb30d6cd8ff4f
cstandre <caitlynstandre@gmail.com>
2023-05-21 18:50:28 -0700

    Revert "Seeded review images."
    This reverts commit 25fee7ee8c48ce88ba606bfcb5f5a0d028af2d48.

--- app/seeds/products.py
+++ app/seeds/products.py
@@ -4,25 +4,11 @@ from sqlalchemy.sql import text
 
 def seed_products():
     product1 = Product(
-        owner_id=1,
-        product_name='Nike Sportswear Club Fleece Women\'s Oversized Crop Graphic Hoodie',
-        price=70.00,
-        brand='Nike',
-        stock_quantity=50,
-        description='Club Fleece sweatshirts, universally loved for their coziness and consistency, are for everyone. Always soft and made with a relaxed fit, they’re basics that help you do more. Cropped to meet your favorite high-waisted pants, this spacious hoodie is perfect for colder days that call for a little extra style.')
+        owner_id=1, product_name='Nike Sportswear Club Fleece Women\'s Oversized Crop Graphic Hoodie', price=70.00, brand='Nike', stock_quantity=50, description='Club Fleece sweatshirts, universally loved for their coziness and consistency, are for everyone. Always soft and made with a relaxed fit, they’re basics that help you do more. Cropped to meet your favorite high-waisted pants, this spacious hoodie is perfect for colder days that call for a little extra style.')
     product2 = Product(
-        owner_id=2,
-        product_name='Nike Women\'s Sneaker Running Shoes',
-        price=122.00, brand='Nike',
-        stock_quantity=100,
-        description='Women\'s running shoes from Nike.')
+        owner_id=2, product_name='Nike Women\'s Sneaker Running Shoes', price=122.00, brand='Nike', stock_quantity=100, description='Women\'s running shoes from Nike.')
     product3 = Product(
-        owner_id=3,
-        product_name='Nike Women\'s NSW Tight Fleece Varsity',
-        price=50.00,
-        brand='Nike',
-        stock_quantity=5,
-        description='The semi-brushed fleece fabric has a soft, lightweight feel perfect for everyday wear')
+        owner_id=3, product_name='Nike Women\'s NSW Tight Fleece Varsity', price=50.00, brand='Nike', stock_quantity=5, description='The semi-brushed fleece fabric has a soft, lightweight feel perfect for everyday wear')
 
     db.session.add(product1)
     db.session.add(product2)
