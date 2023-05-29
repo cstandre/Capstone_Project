8a476dcc1db4b5362078b2273d2c1d6e7e82e42b
cstandre <caitlynstandre@gmail.com>
2023-05-29 13:02:39 -0700

    Create, read, and delete for a product are working. Need to fix update and then full CRUD will be complete.

---
+++ react-app/src/components/Products/DeleteProductModal.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { useDispatch } from "react-redux"
+import { useModal } from "../../context/Modal";
+import { deleteProduct } from "../../store/products";
+
+
+
+const DeleteProductModal = ({ productId }) => {
+    const dispatch = useDispatch();
+    const { closeModal } = useModal();
+
+    const handleClick = async (e) => {
+        e.preventDefault();
+        await dispatch(deleteProduct(productId));
+        closeModal();
+    };
+
+    return (
+        <>
+            <div className="delete-form">
+                <h1 className="delete-title">Confirm Delete</h1>
+                <div className="delete-confirm">Are you sure you want to delete this product?</div>
+                <div className="delete-buttons">
+                <button className="delete-button" onClick={handleClick}>Yes (Delete)</button>
+                <button className="delete-button" onClick={closeModal}>No</button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+
+export default DeleteProductModal
--- react-app/src/components/Products/UserStorePage.js
+++ react-app/src/components/Products/UserStorePage.js
@@ -2,8 +2,8 @@ import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
 import { userProducts } from "../../store/products";
-import { useModal } from "../../context/Modal";
-import { deleteProduct } from "../../store/products";
+import DeleteProductModal from "./DeleteProductModal";
+import OpenModalButton from "../OpenModalButton";
 
 
 
@@ -12,7 +12,6 @@ const UserStorePage = () => {
     const { productId } = useParams();
     const dispatch = useDispatch();
     const history = useHistory();
-    const { closeModal } = useModal();
 
 
     useEffect(() => {
@@ -30,24 +29,24 @@ const UserStorePage = () => {
         history.push(`/products/${productId}/edit`);
     };
 
-    const removeProduct = (e) => {
-        e.preventDefault();
-        const productId = e.target.value;
-        dispatch(deleteProduct(productId));
-    };
-
     return (
         <>
-        {products && (
+        <button onClick={addProduct}>Add</button>
+        {products ? (
             Object.values(products).map((product, idx) =>
             <div key={idx}>
                 <div>{product.product_name}</div>
                 <div>Review Count: {product.reviews?.length}</div>
                 <button value={product.id} onClick={editProduct}>Update</button>
-                <button value={product.id} onClick={removeProduct}>Delete</button>
-                <button onClick={addProduct}>Add</button>
+                <OpenModalButton
+                    buttonText={"Delete"}
+                    modalComponent={<DeleteProductModal productId={product.id} />}
+                />
             </div>
-        ))}
+        )): (
+            <>
+            </>
+        )}
         </>
     )
 };
