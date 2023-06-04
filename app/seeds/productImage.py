942d65ec85669d741d3974b30b29e56808808df7
cstandre <caitlynstandre@gmail.com>
2023-06-04 14:35:37 -0700

    added more product seeders.

--- app/seeds/productImage.py
+++ app/seeds/productImage.py
@@ -3,15 +3,51 @@ from sqlalchemy.sql import text
 
 
 def seed_product_images():
-    image1 = ProductImage(url='https://res.cloudinary.com/djclmc80y/image/upload/v1684711048/fleece_hoodie_1_szjuir.jpg', preview=True, product_id=1)
-    image2 = ProductImage(url='https://res.cloudinary.com/djclmc80y/image/upload/v1684711047/fleece_hoodie_3_mcgffr.jpg', preview=False, product_id=1)
-    image3 = ProductImage(url='https://res.cloudinary.com/djclmc80y/image/upload/v1684711048/fleece_hoodie_2_vlvbjp.jpg', preview=False, product_id=1)
-    image4 = ProductImage(url='https://res.cloudinary.com/djclmc80y/image/upload/v1684711047/fleece_hoodie_4_bxkkrj.jpg', preview=False, product_id=1)
+    nike_product1_image1 = ProductImage(url='https://res.cloudinary.com/djclmc80y/image/upload/v1684711048/fleece_hoodie_1_szjuir.jpg', preview=True, product_id=1)
+    nike_product1_image2 = ProductImage(url='https://res.cloudinary.com/djclmc80y/image/upload/v1684711047/fleece_hoodie_3_mcgffr.jpg', preview=False, product_id=1)
+    nike_product1_image3 = ProductImage(url='https://res.cloudinary.com/djclmc80y/image/upload/v1684711048/fleece_hoodie_2_vlvbjp.jpg', preview=False, product_id=1)
+    nike_product1_image4 = ProductImage(url='https://res.cloudinary.com/djclmc80y/image/upload/v1684711047/fleece_hoodie_4_bxkkrj.jpg', preview=False, product_id=1)
 
-    db.session.add(image1)
-    db.session.add(image2)
-    db.session.add(image3)
-    db.session.add(image4)
+    nike_product2_image1 = ProductImage(url='https://caitlyn.s3.us-west-2.amazonaws.com/seeder/shoes_1.jpg', preview=True, product_id=2)
+    nike_product2_image2 = ProductImage(url='https://res.cloudinary.com/djclmc80y/image/upload/v1684711047/fleece_hoodie_4_bxkkrj.jpg', preview=False, product_id=1)
+    nike_product2_image3 = ProductImage(url='https://caitlyn.s3.us-west-2.amazonaws.com/seeder/shoes_3.jpg', preview=False, product_id=2)
+    nike_product2_image4 = ProductImage(url='https://caitlyn.s3.us-west-2.amazonaws.com/seeder/shoes_4.jpg', preview=False, product_id=2)
+
+    nike_product3_image1 = ProductImage(url='https://caitlyn.s3.us-west-2.amazonaws.com/seeder/pants_1.jpg', preview=True, product_id=3)
+    nike_product3_image2 = ProductImage(url='https://caitlyn.s3.us-west-2.amazonaws.com/seeder/pants_2.jpg', preview=False, product_id=3)
+    nike_product3_image3 = ProductImage(url='https://caitlyn.s3.us-west-2.amazonaws.com/seeder/fleece_hoodie_3.jpg', preview=False, product_id=3)
+    nike_product3_image4 = ProductImage(url='https://caitlyn.s3.us-west-2.amazonaws.com/seeder/pants_4.jpg', preview=False, product_id=3)
+
+    nike_product4_image1 = ProductImage(url='https://caitlyn.s3.us-west-2.amazonaws.com/hip_pack_1.jpg', preview=True, product_id=4)
+    nike_product4_image2 = ProductImage(url='https://caitlyn.s3.us-west-2.amazonaws.com/hip_pack_2.jpg', preview=False, product_id=4)
+    nike_product4_image3 = ProductImage(url='https://caitlyn.s3.us-west-2.amazonaws.com/hip_pack_3.jpg', preview=False, product_id=4)
+
+    nike_product5_image1 = ProductImage(url='https://caitlyn.s3.us-west-2.amazonaws.com/seeder/tech_swoosh_hat_1.jpg', preview=True, product_id=5)
+    nike_product5_image2 = ProductImage(url='https://caitlyn.s3.us-west-2.amazonaws.com/seeder/tech_swoosh_hat_2.jpg', preview=False, product_id=5)
+
+
+    db.session.add(nike_product1_image1)
+    db.session.add(nike_product1_image2)
+    db.session.add(nike_product1_image3)
+    db.session.add(nike_product1_image4)
+
+    db.session.add(nike_product2_image1)
+    db.session.add(nike_product2_image2)
+    db.session.add(nike_product2_image3)
+    db.session.add(nike_product2_image4)
+
+    db.session.add(nike_product3_image1)
+    db.session.add(nike_product3_image2)
+    db.session.add(nike_product3_image3)
+    db.session.add(nike_product3_image4)
+
+    db.session.add(nike_product4_image1)
+    db.session.add(nike_product4_image2)
+    db.session.add(nike_product4_image3)
+
+    db.session.add(nike_product5_image1)
+    db.session.add(nike_product5_image2)
+
     db.session.commit()
 
 
@@ -19,10 +55,28 @@ def undo_product_images():
     if environment == "production":
         db.session.execute(f"TRUNCATE table {SCHEMA}.productImages RESTART IDENTITY CASCADE;")
     else:
-        db.session.delete(image1)
-        db.session.delete(image2)
-        db.session.delete(image3)
-        db.session.delete(image4)
+        db.session.delete(nike_product1_image1)
+        db.session.delete(nike_product1_image2)
+        db.session.delete(nike_product1_image3)
+        db.session.delete(nike_product1_image4)
+
+        db.session.delete(nike_product2_image1)
+        db.session.delete(nike_product2_image2)
+        db.session.delete(nike_product2_image3)
+        db.session.delete(nike_product2_image4)
+
+        db.session.delete(nike_product3_image1)
+        db.session.delete(nike_product3_image2)
+        db.session.delete(nike_product3_image3)
+        db.session.delete(nike_product3_image4)
+
+        db.session.delete(nike_product4_image2)
+        db.session.delete(nike_product4_image1)
+        db.session.delete(nike_product4_image3)
+
+        db.session.delete(nike_product5_image1)
+        db.session.delete(nike_product5_image2)
+
         db.session.execute(text("DELETE FROM productImages"))
 
     db.session.commit()
--- app/seeds/products.py
+++ app/seeds/products.py
@@ -3,16 +3,24 @@ from sqlalchemy.sql import text
 
 
 def seed_products():
-    product1 = Product(
+    nike_product_1 = Product(
         owner_id=1, product_name='Nike Sportswear Club Fleece Women\'s Oversized Crop Graphic Hoodie', price=70.00, brand='Nike', stock_quantity=50, description='Club Fleece sweatshirts, universally loved for their coziness and consistency, are for everyone. Always soft and made with a relaxed fit, they’re basics that help you do more. Cropped to meet your favorite high-waisted pants, this spacious hoodie is perfect for colder days that call for a little extra style.')
-    product2 = Product(
-        owner_id=2, product_name='Nike Women\'s Sneaker Running Shoes', price=122.00, brand='Nike', stock_quantity=100, description='Women\'s running shoes from Nike.')
-    product3 = Product(
-        owner_id=3, product_name='Nike Women\'s NSW Tight Fleece Varsity', price=50.00, brand='Nike', stock_quantity=5, description='The semi-brushed fleece fabric has a soft, lightweight feel perfect for everyday wear')
+    nike_product_2 = Product(
+        owner_id=1, product_name='Nike Women\'s Sneaker Running Shoes', price=122.00, brand='Nike', stock_quantity=100, description='Women\'s running shoes from Nike.')
+    nike_product_3 = Product(
+        owner_id=1, product_name='Nike Women\'s NSW Tight Fleece Varsity', price=50.00, brand='Nike', stock_quantity=5, description='The semi-brushed fleece fabric has a soft, lightweight feel perfect for everyday wear')
+    nike_product_4 = Product(
+        owner_id=1, product_name='Nike Tech Hip Pack', price=58.00, brand='Nike', stock_quantity=50, description='The Nike Tech Hip Pack lets you easily access and carry your gear. It features 2 zippered pockets to store your stuff and an adjustable strap allowing you to customize your fit.'
+    )
+    nike_product_5 = Product(
+        owner_id=1, product_name='Nike Legacy 91 Tech Swoosh Hat', price=32.30, brand='Nike', stock_quantity=400, description='CLASSIC COMFORT. The Nike Dri-FIT Legacy91 Hat is a comfortable course staple with a metal buckle closure and a soft sweatband. This product is made with at least 50% recycled polyester fibers. Benefits Nike Dri-FIT technology moves sweat away from your skin for quicker evaporation, helping you stay dry and comfortable. Sweatband is soft and absorbent. Adjustable closure lets you personalize your fit.'
+    )
 
-    db.session.add(product1)
-    db.session.add(product2)
-    db.session.add(product3)
+    db.session.add(nike_product_1)
+    db.session.add(nike_product_2)
+    db.session.add(nike_product_3)
+    db.session.add(nike_product_4)
+    db.session.add(nike_product_5)
     db.session.commit()
 
 
@@ -20,9 +28,11 @@ def undo_products():
     if environment == "production":
         db.session.execute(f"TRUNCATE table {SCHEMA}.products RESTART IDENTITY CASCADE;")
     else:
-        db.session.delete(product1)
-        db.session.delete(product2)
-        db.session.delete(product3)
+        db.session.delete(nike_product_1)
+        db.session.delete(nike_product_2)
+        db.session.delete(nike_product_3)
+        db.session.delete(nike_product_4)
+        db.session.delete(nike_product_5)
         db.session.execute(text("DELETE FROM products"))
 
     db.session.commit()
