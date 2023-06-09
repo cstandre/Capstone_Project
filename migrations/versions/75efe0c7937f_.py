11d1130e341111f7f4f1b8f26db8582d6bfd0d78
cstandre <caitlynstandre@gmail.com>
2023-06-08 19:23:27 -0700

    trying one more thing.

--- migrations/versions/75efe0c7937f_.py
+++ migrations/versions/75efe0c7937f_.py
@@ -76,7 +76,7 @@ def upgrade():
     )
 
     if environment == "production":
-        op.execute(f"ALTER TABLE cartitems SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE cart_items SET SCHEMA {SCHEMA};")
 
     op.create_table('productImages',
     sa.Column('id', sa.Integer(), nullable=False),
@@ -87,7 +87,7 @@ def upgrade():
     sa.PrimaryKeyConstraint('id')
     )
     if environment == "production":
-        op.execute(f"ALTER TABLE productimages SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE product_images SET SCHEMA {SCHEMA};")
 
     op.create_table('reviews',
     sa.Column('id', sa.Integer(), nullable=False),
@@ -113,7 +113,7 @@ def upgrade():
     )
 
     if environment == "production":
-        op.execute(f"ALTER TABLE reviewimages SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE review_images SET SCHEMA {SCHEMA};")
     # ### end Alembic commands ###
 
 
