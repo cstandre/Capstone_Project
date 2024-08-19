7036ab48b2aed56dc5d257ad809951e651fa0532
cstandre <caitlynstandre@gmail.com>
2024-08-19 15:10:36 -0700

    Updated react, babel, and my image slider.

--- react-app/src/components/main/index.js
+++ react-app/src/components/main/index.js
@@ -1,51 +1,55 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
-import Carousel from '@itseasy21/react-elastic-carousel';
 import { loadProducts } from "../../store/products";
 import { addItem } from "../../store/cart";
-import LoginMessage from "../ ErrorModals/loginModal";
-
-import "./main.css"
+import LoginMessage from "../ ErrorModals/loginModal"
+import Flickity from 'react-flickity-component';
 import OpenModalButton from "../OpenModalButton";
+
+import "./main.css";
+import "flickity/css/flickity.css";
+
+const flickityOptions = {
+  initialIndex: 0,
+  autoPlay: 3000, // Add a delay for better transitions
+  wrapAround: true,
+  pageDots: false,
+  prevNextButtons: true,
+  draggable: true, // Ensure this is set to true
+  freeScroll: false // Prevents scrolling beyond the last slide
+};
+
 
 const MainPage = () => {
   const history = useHistory();
-  const products = useSelector(state=>state?.products);
-  const sessionUser = useSelector(state=>state?.session.user);
+  const products = useSelector((state) => state?.products);
+  const sessionUser = useSelector((state) => state?.session.user);
   const dispatch = useDispatch();
-  const [sliderWidth, setSliderWidth] = useState(0);
-  const [sliderHeight, setSliderHeight] = useState(0);
   const [randomProducts, setRandomProducts] = useState([]);
 
   useEffect(() => {
     dispatch(loadProducts());
   }, [dispatch]);
 
-  useEffect(() => {
-    const calculateSliderSize = () => {
-      const parentContainerWidth = document.querySelector('.body').clientWidth;
-      const calculatedWidth = Math.round(parentContainerWidth * 0.8);
-      setSliderWidth(calculatedWidth);
+  // useEffect(() => {
+  //   const calculateSliderSize = () => {
+  //     const parentContainerWidth = document.querySelector('.body').clientWidth;
+  //     const calculatedWidth = Math.round(parentContainerWidth * 0.8);
+  //     setSliderWidth(calculatedWidth);
+  //     setSliderHeight(Math.round(calculatedWidth * 0.3));
+  //   };
 
-      const calculatedHeight = Math.round(calculatedWidth * 0.3);
-      setSliderHeight(calculatedHeight);
-    };
-
-    calculateSliderSize();
-    window.addEventListener('resize', calculateSliderSize);
-
-    return () => {
-      window.removeEventListener('resize', calculateSliderSize);
-    };
-  }, []);
+  //   calculateSliderSize();
+  //   window.addEventListener('resize', calculateSliderSize);
+  //   return () => window.removeEventListener('resize', calculateSliderSize);
+  // }, []);
 
   useEffect(() => {
-    const productsArr = Object?.values(products)?.flatMap(product => Object?.values(product));
+    const productsArr = Object.values(products)?.flatMap(product => Object.values(product));
     const randomProducts = productsArr?.sort(() => Math.random() - Math.random()).slice(0, 5);
     setRandomProducts(randomProducts);
-    // console.log(randomProducts)
-  }, [products])
+  }, [products]);
 
   const images = [
     {
@@ -67,73 +71,68 @@ const MainPage = () => {
   };
 
   const handleProductDetail = (productId) => {
-    // console.log(productId)
     history.push(`/products/${productId}`);
   };
 
   return (
     <div className="body">
-      <div>
-        {/* <div className="slider">
-          <Carousel
-            itemsToShow={1}
-            itemsToScroll={1}
-            showArrows={true}
-            autoPlay={true}
-            infiniteLoop={true}
-            transitionMs={1000}
-          >
-            {images.map(image => (
-              <div key={image.id}><img src={image.url} alt={`Banner ${image.id}`} /></div>
-            ))}
-          </Carousel>
-        </div> */}
-        <div className="product-display-container">
-          {randomProducts?.map((product, idx) => (
-            <div className={`product-box container-${idx}`} key={idx}>
-              <img
-                className={`product-box-img-${idx}`}
-                alt=""
-                src={product?.preview_image}
-                value={product?.id}
+      <Flickity
+        className={"carousel"}
+        elementType={"div"}
+        options={flickityOptions}
+        disableImagesLoaded={false}
+        reloadOnUpdate
+      >
+        {images.map((image) => (
+          <img
+            className="carousel-img"
+            key={image.id}
+            src={image.url}
+            alt={`Slide ${image.id}`}
+          />
+        ))}
+      </Flickity>
+      <div className="product-display-container">
+        {randomProducts.map((product, idx) => (
+          <div className={`product-box container-${idx}`} key={product.id}>
+            <img
+              className={`product-box-img-${idx}`}
+              alt={product.product_name}
+              src={product?.preview_image}
+              onClick={() => handleProductDetail(product?.id)}
+            />
+            <div className="product-details-main">
+              <p
+                className="product-name-main"
                 onClick={() => handleProductDetail(product?.id)}
-              ></img>
-              <div className="product-details-main">
-                <p
-                  className="product-name-main"
-                  value={product?.id}
-                  onClick={() => handleProductDetail(product?.id)}
-                >
-                  {product.product_name}
-                </p>
-                  {product?.stock_quantity > 0 ? (
-                    <p className="in-stock">In Stock</p>
-                  ) : (
-                    <p className="no-stock">Out of Stock</p>
-                  )}
-              </div>
-              {sessionUser ? (
-                <button
-                  value={product?.id}
-                  onClick={() => handleCartButton(product?.id)}
-                  className="add-to-cart"
-                >
-                  Add to cart
-                </button>
+              >
+                {product.product_name}
+              </p>
+              {product?.stock_quantity > 0 ? (
+                <p className="in-stock">In Stock</p>
               ) : (
-                  <OpenModalButton
-                    className="add-to-cart"
-                    buttonText={'Add to cart'}
-                    modalComponent={<LoginMessage />}
-                  />
+                <p className="no-stock">Out of Stock</p>
               )}
             </div>
-          ))}
-        </div>
+            {sessionUser ? (
+              <button
+                onClick={() => handleCartButton(product?.id)}
+                className="add-to-cart"
+              >
+                Add to cart
+              </button>
+            ) : (
+              <OpenModalButton
+                className="add-to-cart"
+                buttonText={"Add to cart"}
+                modalComponent={<LoginMessage />}
+              />
+            )}
+          </div>
+        ))}
       </div>
     </div>
   );
-
 };
 
 export default MainPage;
